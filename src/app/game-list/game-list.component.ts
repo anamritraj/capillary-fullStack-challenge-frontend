547befcdb0a164ad4e47027e6b16c599b4711bd1
anamritraj <anandamritraj904@gmail.com>
2017-03-18 21:13:40 +0530

    Pagination complete.

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {GameService} from "../services/game.service";
-import {forEach} from "@angular/router/src/utils/collection";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-game-list',
@@ -12,19 +12,26 @@ export class GameListComponent implements OnInit {
 
   games = [];
   search_games = [];
+  onpage_games = [];
+  current_page: number;
+  total_pages:number;
   search_term: string;
   private per_page = 12;
 
   constructor(
-    private _gameService: GameService
+    private _gameService: GameService,
   ) {
     this.search_term = "";
+    this.current_page = 1;
   }
 
   ngOnInit() {
+
     this._gameService.getGames().subscribe(result =>{
       this.games = result;
       this.search_games = result;
+      this.total_pages = Math.ceil(this.search_games.length/this.per_page);
+      this.showPageResults(this.current_page);
     },
     error =>{
       console.log(error);
@@ -32,13 +39,14 @@ export class GameListComponent implements OnInit {
   }
 
   performSearch(){
-    console.log(this.search_term);
     this.search_term = this.search_term.toLowerCase();
+    // console.log(this.search_term);
+    this.current_page = 1;
     this.search_games = [];
     for (let i = 0; i < this.games.length; i++)
     {
       if(this.games[i].title.toLowerCase().includes(this.search_term)){
-        console.log(this.games[i].title);
+        // console.log(this.games[i].title);
         this.search_games.push(this.games[i]);
       }
     }
@@ -47,10 +55,25 @@ export class GameListComponent implements OnInit {
     }
   }
 
+  showPageResults(page_no){
+    console.log(page_no);
+    if(page_no <= this.total_pages && page_no >= 1){
+      this.onpage_games = [];
+      this.current_page = page_no;
+
+      for (let i = this.per_page * (this.current_page - 1); i < this.per_page * this.current_page; i++)
+      {
+        if(this.search_games[i]){
+          this.onpage_games.push(this.search_games[i]);
+        }
+      }
+    }
+  }
+
   createRange(){
-    let pages = Math.ceil(this.search_games.length/this.per_page);
+    this.total_pages = Math.ceil(this.search_games.length/this.per_page);
     let items: number[] = [];
-    for(let i = 1; i <= pages; i++){
+    for(let i = 1; i <= this.total_pages; i++){
       items.push(i);
     }
     return items;
